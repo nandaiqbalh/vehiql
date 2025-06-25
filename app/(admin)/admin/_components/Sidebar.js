1d37bafae9470634b85db19245ac282e8200029d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2025-06-25 20:13:43 +0700

    Feat: - Admin Test Drives

--- app/(admin)/admin/_components/Sidebar.js
+++ app/(admin)/admin/_components/Sidebar.js
@@ -19,7 +19,7 @@ const routes = [
     {
         label: "Test Drives",
         icon: Calendar,
-        href: "/admin/test-drivers",
+        href: "/admin/test-drives",
     },
     {
         label: "Settings",
