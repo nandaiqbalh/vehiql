bae8075273a37b4ed52e9eea7d93d880258cd9ce
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2025-06-25 20:38:10 +0700

    Fix: fixing uncaught import page

--- app/(admin)/admin/page.js
+++ app/(admin)/admin/page.js
@@ -1,5 +1,5 @@
 import { getDashboardData } from "@/actions/admin";
-import { Dashboard } from "./_components/dashboard";
+import {Dashboard} from "@/app/(admin)/admin/_components/Dashboard";
 
 export const metadata = {
     title: "Dashboard | Vehiql Admin",
